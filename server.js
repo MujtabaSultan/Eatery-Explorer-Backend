fc768f6caa5c9577ef8a8af7d1e4e76981a69ce7
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2025-08-03 19:15:32 +0300

    checkong the db

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-const mongoose = require("mongoose");
 const testJWTRouter = require("./controllers/test-jwt");
 console.log("app");
 const usersRouter = require("./controllers/users");
@@ -29,6 +28,9 @@ mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
 app.use("/test-jwt", testJWTRouter);
